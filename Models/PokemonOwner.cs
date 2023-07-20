238bcc1c3bf9755b92f25a9b2b08782d146b5970
Somohano <jaime.somohano@aspentech.com>
2023-07-20 14:39:15 -0600

    first commit

---
+++ Models/PokemonOwner.cs
@@ -0,0 +1,10 @@
+namespace PokemonAPI.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public int CategoryId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
